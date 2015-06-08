ead645432bf38faaa9ba0dc81cbb2e83f4f2de1a
unknown <pgangad@DEBO.pune.tavisca.com>
2015-06-08 13:46:18 +0530

    changes in tut.jsx

--- ReactDemo/Scripts/Tutorial.jsx
+++ ReactDemo/Scripts/Tutorial.jsx
@@ -1,1 +1,39 @@
-﻿
+﻿var CommentBox = React.createClass({
+render : function(){
+	return (
+		<div class="commentBox">
+			<h1>Comments</h1>
+			<CommentList />
+			<CommentForm />
+		</div>
+	);
+}
+});
+
+var CommentList = React.createClass({
+	render:function(){	
+	return(
+		<div class="commentList">
+			Hi there ... I am Comment List ..!!
+		</div>
+	);
+	}
+});
+
+
+var CommentForm = React.createClass({
+	render : function(){
+		return (
+		<div class="commentForm">
+	 		Hey... I am the comment form..!!!
+		</div>
+		);
+	}
+});
+
+React.render(
+<CommentBox />,
+document.getElementById('content')
+);
+
+
